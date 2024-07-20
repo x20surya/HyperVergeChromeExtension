fd659ae63d88b6808b8e69febb5dddf216f96858
x20surya <suryanshuc659@gmail.com>
2024-07-20 08:10:37 +0530

    Add Dark mode toggle

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,18 @@
 import { AlertDialogDemo } from "./components/demoComponents/alert";
+import { ModeToggle } from "./components/demoComponents/darkmodetoggle";
 import { Button } from "./components/ui/button";
+import { ThemeProvider } from "@/components/theme-provider";
 
 function App() {
   return (
-    <div className=" h-96 w-96">
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-      <Button>Hello</Button>
-      <AlertDialogDemo/>
-    </div>
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <div className=" h-96 w-96 flex">
+        <h1 className="text-3xl font-bold underline">Hello world!</h1>
+        <Button>Hello</Button>
+        <AlertDialogDemo />
+        <ModeToggle/>
+      </div>
+    </ThemeProvider>
   );
 }
 

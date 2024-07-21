3679fb358a3f724cac514f846ebd54947c7176d7
x20surya <suryanshuc659@gmail.com>
2024-07-21 21:08:21 +0530

    Update Opportunity UI

--- client/src/components/Widgets/Opportunities/OpportunityCreate.jsx
+++ client/src/components/Widgets/Opportunities/OpportunityCreate.jsx
@@ -1,64 +1,73 @@
-import { useState } from "react"
+import { useState } from "react";
 import {
-    Card,
-    CardContent,
-    CardDescription,
-    CardFooter,
-    CardHeader,
-    CardTitle,
-  } from "@/components/ui/card"
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { Textarea } from "@/components/ui/textarea";
+
+
 
 const OpportunityCreate = (props) => {
-    const {setCreate, getData} = props
-    const [name, setName] = useState("")
-    const [des, setDes] = useState("")
+  const { setCreate, getData } = props;
+  const [name, setName] = useState("");
+  const [des, setDes] = useState("");
 
-    const handleSubmit = (e) => {
-        e.preventDefault()
-        console.log("New Opp Uploaded")
-        console.log(name)
-        console.log(des)
-        const date = new Date()
-        const time = date.getTime()
-        const obj = {
-            title : name,
-            description : des,
-            time,
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("New Opp Uploaded");
+    console.log(name);
+    console.log(des);
+    const date = new Date();
+    const time = date.getTime();
+    const obj = {
+      title: name,
+      description: des,
+      time,
+    };
+    if (name === "" || des === "") {
+      return;
+    } else {
+      fetch(
+        "https://script.google.com/macros/s/AKfycby3NJOvuhFSAgdCErjFmacekmFXSJVigcdvsfThR9zlUZf9boMI1z1pHAxUDMXckOYkXQ/exec?action=addOpp",
+        {
+          method: "POST",
+          body: JSON.stringify(obj),
         }
-        if(name === "" || des === ""){
-            return
-        }else{
-            fetch("https://script.google.com/macros/s/AKfycby3NJOvuhFSAgdCErjFmacekmFXSJVigcdvsfThR9zlUZf9boMI1z1pHAxUDMXckOYkXQ/exec?action=addOpp", {
-                method : "POST",
-                body : JSON.stringify(obj)
-            }).then(()=>{
-                console.log("added new Opp")
-            })
-            setCreate(true)
-            getData()
-        }
+      ).then(() => {
+        console.log("added new Opp");
+      });
+      setCreate(true);
+      getData();
     }
+  };
 
-    return(
-        <>
-            <form
-            onSubmit={handleSubmit}>
-                <input
-                    placeholder="Title"
-                        onChange={(e)=>{setName(e.target.value)}}
-                    value={name}
-                />
-                <input
-                    placeholder="Description"
-                    onChange={(e)=>{setDes(e.target.value)}}
-                    value={des}
-                />
-                <button
-                    onClick={handleSubmit}
-                >Post Opportunity</button>
-            </form>
-
-        </>
-    )
-}
-export default OpportunityCreate
+  return (
+    <>
+      <form onSubmit={handleSubmit} className="flex flex-col h-[70%] justify-between p-5 items-center">
+        <Input
+          placeholder="Title"
+          onChange={(e) => {
+            setName(e.target.value);
+          }}
+          value={name}
+        />
+        <Textarea
+            placeholder="Description"
+            onChange={(e)=>{
+                setDes(e.target.value)
+            }}
+            value={des}
+        />
+        <Button onClick={handleSubmit}>Post Opportunity</Button>
+        <Button variant="outline" onClick={()=>{setCreate(true)}}>Back</Button>
+      </form>
+    </>
+  );
+};
+export default OpportunityCreate;
